d7a7e7740ea05f609985be0f4cc08f41070dc24d
Joaquin Harless <joaquin.harless@gmail.com>
2023-03-01 05:05:07 +0000

    Added 'ACTIONS' list to app.py, used FLASK to render objects into html elements (buttons), used FLASK to conditionally format and style buttons from separate html files

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ACTIONS = [
+  {
+    'id': 1,
+    'name': 'Move',
+    'cost': 1,
+    'difficulty': 'Easy'
+  },
+  {
+    'id': 2,
+    'name': 'Interact',
+    'cost': 2,
+    'difficulty': 'Medium'
+  },
+  {
+    'id': 3,
+    'name': 'Search',
+    'cost': 3,
+    'difficulty': 'Medium'
+  },
+  {
+    'id': 4,
+    'name': 'Attack',
+    'cost': 4,
+    'difficulty': 'Hard'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', actions=ACTIONS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
