6a8c900e7c264af3f44714db292757ffb1ddb44e
Joaquin Harless <joaquin.harless@gmail.com>
2023-03-10 03:44:51 +0000

    Finished database connection; successfully queried database; made webpage load dynamic content!

--- app.py
+++ app.py
@@ -1,37 +1,22 @@
 from flask import Flask, render_template, jsonify
-
+from database import engine
+from sqlalchemy import text
 app = Flask(__name__)
 
-ACTIONS = [
-  {
-    'id': 1,
-    'name': 'Move',
-    'cost': 1,
-    'difficulty': 'Trivial'
-  },
-  {
-    'id': 2,
-    'name': 'Interact',
-    'cost': 2,
-    'difficulty': 'Easy'
-  },
-  {
-    'id': 3,
-    'name': 'Search',
-    'cost': 3,
-    'difficulty': 'Medium'
-  },
-  {
-    'id': 4,
-    'name': 'Attack',
-    'cost': 4,
-    'difficulty': 'Hard'
-  }
-]
+def load_actions_from_db():
+  #use the database engine to open a connection inside the variable "conn" to the database and query the database; save the results in the variable "result"
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM actions"))
+  #create a list of dictionaries; each dictionary is a row in the "actions" table
+    actions = []
+    for row in result.all():
+      actions.append(row._asdict())
+    return actions
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', actions=ACTIONS)
+  actions_list = load_actions_from_db()
+  return render_template('home.html', actions=actions_list)
 
 @app.route("/api/actions")
 def list_actions():
--- database.py
+++ database.py
@@ -1,17 +1,19 @@
 from sqlalchemy import create_engine, text
 
+#save the database connection string as a variable
 db_conn_str = "mysql+pymysql://lilctga62bk09z3ipajt:pscale_pw_R1d9qUNhmLEN0aNvNNdNOgCWmZ4q2m6D0MxsCcJX9Lt@us-west.connect.psdb.cloud/thehoo?charset=utf8mb4"
 
-engine = create_engine(db_conn_str, connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
+#creating an database engine using db_conn_str inside of a variable
+engine = create_engine(db_conn_str,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+  
+  #save all results in variable
+  #result_all = result.all()
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from actions"))
-  print("type(result): ", type(result))
-  result_all = result.all()
-  print("type(result.all()): ", type(result_all))
-  first_result = result_all[0]
-  print("First result: ", first_result)
-  print("First result type: ", type(first_result))
-  first_result_dict = dict(result_all[0])
-  print("first_result_dict type: ", type(first_result_dict))
-  print(first_result_dict)
+  #save first result in result_all list in a variable
+  #first_result = result_all[0]
+
+  #create a dictionary with the first result
+  #first_result_dict = first_result._asdict()
